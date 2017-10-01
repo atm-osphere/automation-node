35ba66ea8b99d764003df3a4855ff1eff79ecf4a
Christian Dupuis <cd@atomist.com>
2017-10-01 22:07:28 +0200

    updates to make NewAutomation work

--- src/commands/generator/NewAutomation.ts
+++ src/commands/generator/NewAutomation.ts
@@ -21,6 +21,7 @@ export class NewAutomation extends UniversalSeed {
 
     constructor() {
         super();
+        this.sourceOwner = "atomist";
         this.sourceRepo = "automation-client-samples-ts";
     }
 
